78871d16818d3dc431833b754a72b97b4e7f29f8
Kai deRochemont and monique st laurent <kai@>
2017-04-18 10:05:11 -0700

    create string method that returns the point value for one-point letters

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,4 +1,13 @@
 class String
   define_method(:scrabble_score) do
-
+    letters = self.downcase().split("")
+    score = 0
+    one_point = []
+    letters.each() do |letter|
+      if letter.include?('a') or letter.include?('e') or letter.include?('i') or letter.include?('o') or letter.include?('u') or letter.include?('r') or letter.include?('s') or letter.include?('t')
+        one_point.push(letter)
+      end
+    end
+   one_point.length()
+  end
 end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -4,6 +4,8 @@ require('scrabble_score')
 
 describe('String#scrabble_score') do
 
-
+  it("returns one point for letters: A E I O U L N R S T") do
+    expect("a".scrabble_score()).to(eq(1))
+  end
 
 end
