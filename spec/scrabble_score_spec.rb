85a1af863d386d182894a1d685886c961d4a2b7e
Kai deRochemont and monique st laurent <kai@>
2017-04-18 10:39:53 -0700

    add functionality to scrabble_score method to ignore special characters and letter case

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -4,12 +4,20 @@ require('scrabble_score')
 
 describe('String#scrabble_score') do
 
-  it("returns a scrabble score for a letter") do
+  it("returns a scrabble score for an lowercase letter") do
     expect("a".scrabble_score()).to(eq(1))
+  end
+
+  it("returns a scrabble score for a uppercase letter") do
+    expect("A".scrabble_score()).to(eq(1))
   end
 
   it("returns the score for a multiple letter word") do
     expect("dog".scrabble_score()).to(eq(5))
+  end
+
+  it("ignores non-letter characters and returns the score for a multiple letter word") do
+    expect("dog@))*****".scrabble_score()).to(eq(5))
   end
 
 end
