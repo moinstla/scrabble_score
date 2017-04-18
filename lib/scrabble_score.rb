47af91a078e97c2bcc90d9c2f80847766ed01bf5
Kai deRochemont and monique st laurent <kai@>
2017-04-18 10:26:29 -0700

    add functionality to return total score of word in scrabble_score string method

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,13 +1,31 @@
 class String
   define_method(:scrabble_score) do
     letters = self.downcase().split("")
-    score = 0
     one_point = []
+    two_point = []
+    three_point = []
+    four_point = []
+    five_point = []
+    eight_point = []
+    ten_point = []
     letters.each() do |letter|
       if letter.include?('a') or letter.include?('e') or letter.include?('i') or letter.include?('o') or letter.include?('u') or letter.include?('r') or letter.include?('s') or letter.include?('t')
         one_point.push(letter)
+      elsif letter.include?('d') or letter.include?('g')
+        two_point.push(letter)
+      elsif letter.include?('b') or letter.include?('c') or letter.include?('m') or letter.include?('p')
+        three_point.push(letter)
+      elsif letter.include?('f') or letter.include?('h') or letter.include?('v') or letter.include?('w') or letter.include?('y')
+        four_point.push(letter)
+      elsif letter.include?('k')
+        five_point.push(letter)
+      elsif letter.include?('j') or letter.include?('x')
+        eight_point.push(letter)
+      elsif letter.include?('q') or letter.include?('z')
+        ten_point.push(letter)
       end
     end
-   one_point.length()
+    score = one_point.length() + two_point.length().*(2) + three_point.length().*(3) + four_point.length().*(4) + five_point.length().*(5) + eight_point.length().*(8) + ten_point.length().*(10)
+   score
   end
 end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -4,8 +4,12 @@ require('scrabble_score')
 
 describe('String#scrabble_score') do
 
-  it("returns one point for letters: A E I O U L N R S T") do
+  it("returns a scrabble score for a letter") do
     expect("a".scrabble_score()).to(eq(1))
+  end
+
+  it("returns the score for a multiple letter word") do
+    expect("dog".scrabble_score()).to(eq(5))
   end
 
 end
