19eccc68a23c156bb6dcda24425daefa95c7a239
Kai deRochemont and monique st laurent <kai@>
2017-04-18 10:55:18 -0700

    refactor letter checks using the match string method

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,31 +1,24 @@
 class String
   define_method(:scrabble_score) do
     letters = self.downcase().split("")
-    one_point = []
-    two_point = []
-    three_point = []
-    four_point = []
-    five_point = []
-    eight_point = []
-    ten_point = []
+    score = 0
     letters.each() do |letter|
-      if letter.include?('a') or letter.include?('e') or letter.include?('i') or letter.include?('o') or letter.include?('u') or letter.include?('r') or letter.include?('s') or letter.include?('t')
-        one_point.push(letter)
-      elsif letter.include?('d') or letter.include?('g')
-        two_point.push(letter)
-      elsif letter.include?('b') or letter.include?('c') or letter.include?('m') or letter.include?('p')
-        three_point.push(letter)
-      elsif letter.include?('f') or letter.include?('h') or letter.include?('v') or letter.include?('w') or letter.include?('y')
-        four_point.push(letter)
-      elsif letter.include?('k')
-        five_point.push(letter)
-      elsif letter.include?('j') or letter.include?('x')
-        eight_point.push(letter)
-      elsif letter.include?('q') or letter.include?('z')
-        ten_point.push(letter)
+      if letter.match /a|e|i|o|u|l|n|r|s|t/
+        score += 1
+      elsif letter.match /d|g/
+        score += 2
+      elsif letter.match /b|c|m|p/
+        score += 3
+      elsif letter.match /f|h|v|w|y/
+        score += 4
+      elsif letter.match /k/
+        score += 5
+      elsif letter.match /j|x/
+        score += 8
+      elsif letter.match /q|z/
+       score += 10
       end
     end
-    score = one_point.length() + two_point.length().*(2) + three_point.length().*(3) + four_point.length().*(4) + five_point.length().*(5) + eight_point.length().*(8) + ten_point.length().*(10)
    score
   end
 end
